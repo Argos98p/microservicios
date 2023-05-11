f6114f3586277869bc70327dc4ee7fea68945efa
Ricardo <jaio1998619@gmail.com>
2023-05-11 16:25:20 -0500

    jwt

--- auth-service/src/main/java/com/turisup/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/turisup/authservice/AuthServiceApplication.java
@@ -1,14 +1,13 @@
-package com.turisup.gatewayservice;
+package com.turisup.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GatewayServiceApplication {
+public class AuthServiceApplication {
 
 	public static void main(String[] args) {
-
-		SpringApplication.run(GatewayServiceApplication.class, args);
+		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
 }
