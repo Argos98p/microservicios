bc170a76dbfa289407d73a20a2564e88842ae32e
Ricardo <jaio1998619@gmail.com>
2023-06-02 19:38:09 -0500

    updates

--- auth-service/src/main/java/com/turisup/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/turisup/authservice/AuthServiceApplication.java
@@ -2,11 +2,14 @@ package com.turisup.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+@ComponentScan(basePackages = {" com.turisup.userservice"})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(AuthServiceApplication.class, args);
 	}
 
