7b1b3e693382e1ac383a404e8fe3cdac8261891d
Ricardo <jaio1998619@gmail.com>
2023-06-02 20:04:54 -0500

    updates

--- auth-service/src/main/java/com/turisup/authservice/security/SecurityConfig.java
+++ auth-service/src/main/java/com/turisup/authservice/security/SecurityConfig.java
@@ -3,11 +3,25 @@ package com.turisup.authservice.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        http
+                .cors()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf()
+                .disable()
+                .formLogin()
+                .disable()
+                .httpBasic()
+                .disable().authorizeRequests().anyRequest().permitAll();
+
+
     }
 }
